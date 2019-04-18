831536d1d416f4859e6a68e44531a7df58bdedea
Yadunandan Batchu <nandubatchu@gmail.com>
2019-04-18 22:26:15 +0530

    wip for dcrwallet

---
+++ decred/dcrwallet/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for dcrwallet"
+
+  set -- dcrwallet "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "dcrwallet" ]; then
+  mkdir -p "$DECRED_DATA"
+  chmod 700 "$DECRED_DATA"
+
+  echo "$0: setting data directory to $DECRED_DATA"
+
+  set -- "$@" --datadir="$DECRED_DATA"
+fi
+
+echo
+exec "$@"
